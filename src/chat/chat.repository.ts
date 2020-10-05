fd50455668c84e9422883b36b33c600966f45fc2
Vladislav <skyvvader@gmail.com>
2020-10-05 13:18:42 +0300

    throw

--- src/chat/chat.repository.ts
+++ src/chat/chat.repository.ts
@@ -1,13 +1,20 @@
+import { NotFoundException } from '@nestjs/common';
 import { Connection, EntityRepository, Repository } from 'typeorm';
 import { Chat } from './models/chat.entity';
 
 @EntityRepository(Chat)
 export class ChatRepository extends Repository<Chat> {
-  public findById(chatId: number): Promise<Chat> {
-    return this.createQueryBuilder('chat')
+  public async findById(chatId: number, thrw = true): Promise<Chat> {
+    const chat = await this.createQueryBuilder('chat')
       .leftJoinAndSelect('chat.users', 'user')
       .where('chat.id = :chatId', { chatId })
       .getOne();
+
+    if (!chat && thrw) {
+      throw new NotFoundException('Chat not found.');
+    }
+
+    return chat;
   }
 
   public findByUserId(uid: number): Promise<Chat[]> {
