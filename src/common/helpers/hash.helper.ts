e8eda073e4be67ff85d7e2f8547827a75d01f051
Vladislav <skyvvader@gmail.com>
2020-12-02 18:52:46 +0200

    format

--- src/common/helpers/hash.helper.ts
+++ src/common/helpers/hash.helper.ts
@@ -1,9 +1,8 @@
-
 import * as bcrypt from 'bcrypt';
 import { Env } from '../env';
 
 export class HashHelper {
-  static async bcrypt (value: string): Promise<string> {
+  static async bcrypt(value: string): Promise<string> {
     return bcrypt.hash(value, Env.BCRYPT_SALT);
   }
 }
